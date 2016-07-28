4f3e4f43452e2aee204d48a126dfd5549471218a
Devin Osorio <me@devinosor.io>
2016-07-28 13:01:20 -0400

    adds travis CI and Coveralls

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -8,6 +8,8 @@ require 'rspec/rails'
 require 'capybara/rails'
 require 'capybara/rspec'
 require 'support/factory_girl'
+require 'coveralls'
+Coveralls.wear!('rails'
 
 # Add additional requires below this line. Rails is not loaded until this point!
 # Requires supporting ruby files with custom matchers and macros, etc, in
