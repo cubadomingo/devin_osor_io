e0211215765b643a1688e271e227d23c6e218bee
Devin Osorio <me@devinosor.io>
2016-07-28 13:26:53 -0400

    fixes specs error

--- spec/features/nav_links_spec.rb
+++ spec/features/nav_links_spec.rb
@@ -5,15 +5,8 @@ RSpec.feature "Checks navbar links" do
     Capybara.current_session.driver.header('User-Agent', 'Mac')
   end
 
-  scenario "nav bar links are present" do
-    visit root_path
-    expect(page).to have_link "about", href: "/#about"
-    expect(page).to have_link "skills", href: "/#skills"
-    expect(page).to have_link "projects", href: "/#projects"
-    expect(page).to have_link "contact", href: "/#contact"
-  end
-
   scenario "nav bar blog link works" do
+    user = create :user
     visit root_path
     first(:link, "blog").click
     assert page.current_path == blog_path
